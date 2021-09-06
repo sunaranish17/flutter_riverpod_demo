b9fc2c32cdba5e2757bb97680ca33a213d0dad74
Anish <sunaranish17@gmail.com>
2021-09-06 10:10:38 +0545

    Feat: New state provider return bool

--- lib/screens/providers/state_provider.dart
+++ lib/screens/providers/state_provider.dart
@@ -3,6 +3,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final stateProvider = StateProvider.autoDispose<int>((ref) => 0);
 
+final isCountEven = StateProvider.autoDispose<bool>((ref) {
+  final counter = ref.watch(stateProvider);
+  return (counter.state % 2 == 0);
+});
+
 class StateProviderPage extends ConsumerWidget {
   const StateProviderPage({Key? key}) : super(key: key);
 
@@ -10,6 +15,7 @@ class StateProviderPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final provider = ref.watch(stateProvider);
     final counter = provider.state;
+    final isEven = ref.watch(isCountEven).state;
     return Scaffold(
       appBar: AppBar(
         title: Text("State Provider"),
@@ -31,7 +37,8 @@ class StateProviderPage extends ConsumerWidget {
               reset.state = 0;
             },
             child: Text("Reset"),
-          )
+          ),
+          if (counter > 0) Text(isEven ? "Count is even" : "Count is odd"),
         ],
       ),
       floatingActionButton: FloatingActionButton(
